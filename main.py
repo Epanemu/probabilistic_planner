b64c657c10d13c55a86ff40e13e7ab0be7c86fdb
Jiri Nemecek <contact@nemecekjiri.cz>
2022-05-10 21:31:15 +0200

    Add main.py to show some usage

---
+++ main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from maze import Maze
+from engine import Engine
+from ffreplan import FFReplan
+from val_iter import ValueIteration
+from stat_tester import simulate_sequence, multiple_runs, test_some_and_store
+import sys
+import time
+
+def main():
+    test_some_and_store("results/ff_replan_7", "dataset-assignment2/data/", "7", 10000, FFReplan)
+    test_some_and_store("results/ff_replan_15", "dataset-assignment2/data/", "15", 1000, FFReplan)
+    test_some_and_store("results/ff_replan_25", "dataset-assignment2/data/", "25", 1000, FFReplan)
+    test_some_and_store("results/ff_replan_51", "dataset-assignment2/data/", "51", 100, FFReplan)
+    test_some_and_store("results/ff_replan_101", "dataset-assignment2/data/", "101", 10, FFReplan)
+
+    test_some_and_store("results/val_iter_7", "dataset-assignment2/data/", "7", 10000, ValueIteration, gamma=0.99999, lim_residual=1e-10)
+    test_some_and_store("results/val_iter_15", "dataset-assignment2/data/", "15", 10000, ValueIteration, gamma=0.99999, lim_residual=1e-10)
+    test_some_and_store("results/val_iter_25", "dataset-assignment2/data/", "25", 10000, ValueIteration, gamma=0.99999, lim_residual=1e-10)
+    test_some_and_store("results/val_iter_51", "dataset-assignment2/data/", "51", 10000, ValueIteration, gamma=0.99999, lim_residual=1e-10)
+    test_some_and_store("results/val_iter_101", "dataset-assignment2/data/", "101", 10000, ValueIteration, gamma=0.99999, lim_residual=1e-10)
+
+if __name__ == "__main__":
+    main()
